b551b52f0a7f0fb7e2196406db50ece9bb26a1d7
Krompaß, Matthias (ADBV A) <Matthias.Krompass@adbv-a.bayern.de>
2025-12-09 16:13:51 +0100

    Label-Anpassung

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,7 @@
 <h1>STI-Auth</h1>
 <form method="POST" action="{{config('sti-auth.route_login')}}">
     @csrf
-    <input name="username" placeholder="email">
+    <input name="username" placeholder="username">
     <input name="password" type="password" placeholder="password">
     <button>Login</button>
 </form>
